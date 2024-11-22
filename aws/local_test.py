c3bc22f2960932b4ae23e564a721f4ce6341b761
ypurek <oleksii.ostapov@gmail.com>
2024-11-22 16:17:29 +0200

    local test

---
+++ aws/local_test.py
@@ -0,0 +1,10 @@
+from recorder2 import main
+
+USERNAME='test'
+PASSWORD='test'
+LINKS = [
+    'http://test1',
+]
+
+if __name__ == '__main__':
+    main(username=USERNAME, password=PASSWORD, links=LINKS)
